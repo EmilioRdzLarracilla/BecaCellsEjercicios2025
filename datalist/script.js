418592d3ae0774fc9d202de138e43cc9ce1b0133
GagoPianisst <beyerperez123@gmail.com>
2025-03-05 09:50:46 -0600

    Cambios en el JS

--- datalist/script.js
+++ datalist/script.js
@@ -1,7 +1,6 @@
 document.querySelector("#busqueda").addEventListener("click", function() {
     var buscar = document.querySelector("#buscar").value;
 
-    if(buscar) {
-        window.location.href = `https://www.google.com/search?q=${encodeURIComponent(buscar)}`;
-    }
+    window.location.href = `https://www.google.com/search?q=${encodeURIComponent(buscar)}`;
+    
 });
