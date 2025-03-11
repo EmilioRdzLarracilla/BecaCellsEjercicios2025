e206defcfec22da8b3b7cb5198a55ba9ad7f76cd
GagoPianisst <beyerperez123@gmail.com>
2025-03-11 09:38:09 -0600

    js

---
+++ 10-marzo-2025/index.js
@@ -0,0 +1,47 @@
+var dinero;
+
+let carro = 'BMW'; // Tokens, let, carro, =, 'BMW'
+                   // format control character => ; \n \r
+                   // whitespaces no importan
+
+// Ejemplo de statement expression
+
+let persona = 'Nestor';
+let nombrePersona = [1, 2, 3, 4].map((i) => {
+    return i*3
+})
+
+true // boolean
+'string' // string
+10 //numero
+{nombre: 'Nestor'} // object
+[{nombre: 'Nestor'} ] // array
+
+function obtenerNombre() {
+    return 'Nestor';
+}
+
+const fun = function() {};
+
+// Variables
+
+function miFuncion() {
+    var dinero = 5;
+    dinero = 10;
+    var dinero;
+    console.log(dinero);
+}
+
+miFuncion();
+var dinero = 'mucho';
+console.log(dinero);
+
+let beyer = 'Beyer';
+
+{
+    let beyer;
+    beyer = 'beyer';
+    console.log(beyer);
+}
+
+console.log(apellido);
